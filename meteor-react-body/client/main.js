c6f3b1c662dce9c92cad0b982fa1e34464378a37
IvanGarL <if.garcia@uniandes.edu.co>
2019-10-12 12:48:34 -0500

    setup starting files

---
+++ meteor-react-body/client/main.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+ 
+Meteor.startup(() => {
+  //render(<WeekForecast />, document.getElementById("root"));
+});
